8d313a419d2c228a04e232f95a11b7edf7a60c49
Byongho <unlike96@gmail.com>
2022-07-19 17:56:38 +0900

    1~56

---
+++ 6012.py
@@ -0,0 +1,8 @@
+'''
+줄을 바꿔 정수(integer) 2개를 입력받아 줄을 바꿔 출력해보자.
+'''
+
+int1 = int(input())
+int2 = int(input())
+print(int1)
+print(int2)
---
+++ 6014.py
@@ -0,0 +1,6 @@
+'''
+실수(real number) 1개를 입력받아 줄을 바꿔 3번 출력해보자.
+'''
+f = input()
+for _ in range(3):
+    print(f)
